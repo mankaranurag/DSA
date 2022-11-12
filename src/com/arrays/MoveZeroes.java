34fb82b599f62630fdf9312a635448824e5fc56d
Anurag Mankar <24831189+mankaranurag@users.noreply.github.com>
2022-11-12 17:55:01 +0530

    Q4

---
+++ src/com/arrays/MoveZeroes.java
@@ -0,0 +1,20 @@
+package com.arrays;
+
+/**
+ * @author Anurag <br />
+ *         <link>https://leetcode.com/problems/move-zeroes/</link>
+ *         LeetCode
+ */
+public class MoveZeroes {
+	public void moveZeroes(int[] nums) {
+        int k = 0;
+        for(int i=0;i<nums.length;i++){
+            if( nums[i] != 0){
+                int temp = nums[k];        
+                nums[k] = nums[i];
+                nums[i] = temp;
+                k++;
+            }
+        }
+    }
+}
--- src/com/arrays/README.md
+++ src/com/arrays/README.md
@@ -2,4 +2,5 @@
 
 1. [Two Sum](https://leetcode.com/problems/two-sum/)
 2. [Best Time to Buy and Sell Stock](https://leetcode.com/problems/best-time-to-buy-and-sell-stock/)
-2. [Merge Sorted Array](https://leetcode.com/problems/merge-sorted-array/)
+3. [Merge Sorted Array](https://leetcode.com/problems/merge-sorted-array/)
+4. [Move Zeroes](https://leetcode.com/problems/move-zeroes/)
