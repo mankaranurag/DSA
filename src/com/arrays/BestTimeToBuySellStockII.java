7350c9f43b6bf6a0f981e2493b9cc5a731695ce2
Anurag Mankar <24831189+mankaranurag@users.noreply.github.com>
2022-11-12 18:05:13 +0530

    Q5

---
+++ src/com/arrays/BestTimeToBuySellStockII.java
@@ -0,0 +1,21 @@
+package com.arrays;
+
+/**
+ * @author Anurag <br />
+ *         <link>https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/</link>
+ *         LeetCode
+ */
+public class BestTimeToBuySellStockII {
+	public int maxProfit(int[] prices) {
+		int maxProfit = 0;
+		int buyPriceDay = 0;
+		for (int i = 1; i < prices.length; i++) {
+			int profit = prices[i] - prices[buyPriceDay];
+			if (profit > 0) {
+				maxProfit += profit;
+			}
+			buyPriceDay = i;
+		}
+		return maxProfit;
+	}
+}
--- src/com/arrays/README.md
+++ src/com/arrays/README.md
@@ -4,3 +4,4 @@
 2. [Best Time to Buy and Sell Stock](https://leetcode.com/problems/best-time-to-buy-and-sell-stock/)
 3. [Merge Sorted Array](https://leetcode.com/problems/merge-sorted-array/)
 4. [Move Zeroes](https://leetcode.com/problems/move-zeroes/)
+4. [Best Time to Buy and Sell Stock II](https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/)
