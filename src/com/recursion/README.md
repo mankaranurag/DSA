a197830c744e1541847a716adc61519714df6ab1
Anurag Mankar <24831189+mankaranurag@users.noreply.github.com>
2022-11-15 10:59:39 +0530

    Recursion Q4

--- src/com/recursion/README.md
+++ src/com/recursion/README.md
@@ -5,3 +5,4 @@
 1. [Pow(x, n)](https://leetcode.com/problems/powx-n/)
 2. [Valid Palindrome](https://leetcode.com/problems/valid-palindrome/)
 3. [Subsets](https://leetcode.com/problems/subsets/)
+4. [Permutations](https://leetcode.com/problems/permutations/)
---
+++ src/com/recursion/q4permutations/Permutations_Recursion.java
@@ -0,0 +1,32 @@
+package com.recursion.q3subsets;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author Anurag <br />
+ *         <link>https://leetcode.com/problems/permutations/link> LeetCode
+ */
+public class Permutations_Recursion {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        permute(nums, res, new ArrayList<>());
+        return res;
+    }
+    
+    private void permute(int []nums,List<List<Integer>> res, List<Integer> list){
+        if(list.size() == nums.length){
+            res.add(new ArrayList<>(list));
+        }else{
+            for(int i=0;i<nums.length;i++){
+                if(!list.contains(nums[i])){
+                    list.add(nums[i]);
+                    permute(nums, res, list);
+                    list.remove(list.size()-1);
+
+                }
+            }
+        }
+    }
+}
