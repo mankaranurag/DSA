872329abe3140a19563ed80eacd20f72c9ee1430
anurag <anurag.mankar0@gmail.com>
2022-12-20 11:23:33 +0530

    Arrays Q27

--- src/com/arrays/CombinationSum.java
+++ src/com/arrays/CombinationSum.java
@@ -13,21 +13,22 @@ public class CombinationSum {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         Arrays.sort(candidates);
         List<List<Integer>> output = new ArrayList<>();
-        combinationSum(candidates, target, 0, 0, output, new ArrayList<>());
+        combinationSum(candidates, target, 0, output, new ArrayList<>());
         return output;
     }
 
-    private void combinationSum(int[] candidates, int target, int current, int currentSum, List<List<Integer>> output, List<Integer> list) {
+    private void combinationSum(int[] candidates, int target, int current, List<List<Integer>> output, List<Integer> list) {
         if (current >= candidates.length) return;
-        if (currentSum > target) return;
-        if (currentSum == target) {
+        if (target == 0) {
             output.add(new ArrayList<>(list));
             return;
         }
         for (int i = current; i < candidates.length; i++) {
-            list.add(candidates[i]);
-            combinationSum(candidates, target, i, currentSum + candidates[i], output, list);
-            list.remove(list.size() - 1);
+            if( candidates[i] <= target) {
+                list.add(candidates[i]);
+                combinationSum(candidates, target - candidates[i], i, output, list);
+                list.remove(list.size() - 1);
+            }
         }
     }
 }
